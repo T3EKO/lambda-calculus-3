05027ba08f69005f66d795cbb8d98f1f77a1589f
T3EKO <graham+github@tenille.net>
2025-05-26 10:00:29 -0400

    initial commit

---
+++ build/CMakeFiles/lambda-calculus-3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lambda-calculus-3.dir/link.d"
+  "CMakeFiles/lambda-calculus-3.dir/main.cpp.o"
+  "CMakeFiles/lambda-calculus-3.dir/main.cpp.o.d"
+  "lambda-calculus-3"
+  "lambda-calculus-3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lambda-calculus-3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from lambda-calculus-3!\n";
+}
